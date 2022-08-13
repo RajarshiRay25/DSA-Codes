86a1b2b98b621a88a9abf375d0e6c5482225620c
Rajarshi Ray <rayrajarshi9@gmail.com>
2022-08-13 19:46:56 +0530

    INSERTION BASIC

---
+++ INSERTION_01_BASIC.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void display(int arr[],int size){
+    printf("ARRAY IS : \n");
+    for(int i=0;i<size;i++){
+        printf("%d \n",arr[i]);
+    }
+    printf("\n");
+}
+
+int insertElem(int arr[],int elem,int size,int capacity,int index){
+    if(size>=capacity){
+        return -1;
+    }
+    else{
+        for(int i = size-1;i>=index;i--){
+            arr[i+1] = arr[i];
+        }
+        arr[index] = elem;
+        return 1;
+    }
+}
+int main(){
+    int arr[100] = {1,33,55,22,674};
+    int size = 5,elem = 23,index = 3,capacity = 100;
+    printf("BEFORE INSERTION. \n");
+    display(arr,size);
+    insertElem(arr,elem,size,capacity,index);
+    size++;
+    printf("AFTER INSERTION. \n");
+    display(arr,size);
+    return 0;
+}
