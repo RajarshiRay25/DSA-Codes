5af5f732d55abaea0716f4ea95f703a10f3f29a4
Rajarshi Ray <rayrajarshi9@gmail.com>
2022-08-13 20:47:34 +0530

    DELETION BASIC

--- DELETION_01_BASIC.c
+++ DELETION_01_BASIC.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+
+// display array
 void display(int arr[],int size){
     printf("ARRAY IS : \n");
     for(int i=0;i<size;i++){
@@ -7,17 +9,20 @@ void display(int arr[],int size){
     printf("\n");
 }
 
+// delete element
 int deleteArr(int arr[],int index,int size){
-    for(int i = index;i<size-1;i++){
-        arr[i] = arr[i+1];
+    for(int i = index;i<size-1;i++){        // from desired index to end of array
+        arr[i] = arr[i+1];                  // assign deleting index with next elements of array so that the given index element is deleted.
     }
 }
 int main(){
     int arr[100] = {10,22,44,66,11};
     int index = 2,size = 5,capacity = 100;
+    printf("BEFORE DELETION \n");
     display(arr,size);
     deleteArr(arr,index,size);
-    size--;
+    size--;                                 // size of array decrease as deletion
+    printf("AFTER DELETION \n");
     display(arr,size);
     return 0;
 }
